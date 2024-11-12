dec11aa7a1a97b9a3cbafe5871f7b90e733efb07
oohyj <hyj0660@gmail.com>
2024-11-12 16:49:09 +0900

    feat : 작가 기본 정보 조회

--- karma/src/main/java/com/ecyce/karma/domain/user/controller/UserController.java
+++ karma/src/main/java/com/ecyce/karma/domain/user/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.ecyce.karma.domain.user.controller;
 
+import com.ecyce.karma.domain.user.dto.response.ArtistInfoResponse;
+import com.ecyce.karma.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+
+    /* 작가 정보 조회 */
+    @GetMapping("/artist/{userId}")
+    public ResponseEntity<ArtistInfoResponse> getArtistInfo(@PathVariable("userId") Long userId){
+        ArtistInfoResponse response = userService.getArtistInfo(userId);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(response);
+    }
 }

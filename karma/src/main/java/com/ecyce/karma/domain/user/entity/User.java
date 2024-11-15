b6fbc79a0bf1ea2ddb9cc04134ec75e9e9046c3d
oohyj <hyj0660@gmail.com>
2024-11-15 14:56:40 +0900

    hot fix : git 충돌 해결

--- karma/src/main/java/com/ecyce/karma/domain/user/entity/User.java
+++ karma/src/main/java/com/ecyce/karma/domain/user/entity/User.java
@@ -5,6 +5,7 @@ import com.ecyce.karma.domain.bookmark.entity.Bookmark;
 import com.ecyce.karma.domain.notice.entity.Notice;
 import com.ecyce.karma.domain.order.entity.Orders;
 import com.ecyce.karma.domain.product.entity.Product;
+import com.ecyce.karma.domain.user.dto.request.UserInfoRequest;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -76,6 +77,20 @@ public class User {
     // 액세스 토큰 업데이트
     public void updateKakaoAccessToken(String kakaoAccessToken) {
         this.kakaoAccessToken = kakaoAccessToken;
+    }
+
+    /* 새로운 회원인 경우 정보 저장*/
+    public void updateNewUserInfo(User user , UserInfoRequest userInfoRequest){
+        this.name = userInfoRequest.name();
+        this.nickname = userInfoRequest.nickname();
+        this.phoneNumber = userInfoRequest.phoneNumber();
+        this.address = Address.builder()
+                .postalCode(userInfoRequest.postalCode())
+                .address1(userInfoRequest.address1())
+                .address2(userInfoRequest.address2())
+                .address3(userInfoRequest.address3())
+                .build();
+
     }
 }
 

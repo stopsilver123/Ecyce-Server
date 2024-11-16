ad5c38086ad25b4ab4fecf1d0eb632512d5a067a
stopsilver123 <sofiajieun@ewha.ac.kr>
2024-11-16 18:00:52 +0900

    fix: 채팅방 조회 시 상대방 이름으로 방 이름 설정

--- karma/src/main/java/com/ecyce/karma/domain/chat/dto/ChatRoomListDto.java
+++ karma/src/main/java/com/ecyce/karma/domain/chat/dto/ChatRoomListDto.java
@@ -2,6 +2,7 @@ package com.ecyce.karma.domain.chat.dto;
 
 import com.ecyce.karma.domain.chat.entity.ChatMessage;
 import com.ecyce.karma.domain.chat.entity.ChatRoom;
+import com.ecyce.karma.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,8 +13,18 @@ public class ChatRoomListDto {
     private String roomName;
     private String latestMessage;
 
-    public static ChatRoomListDto from(ChatRoom chatRoom, ChatMessage latestMessage) {
-        String content = (latestMessage != null) ? latestMessage.getContent() : "채팅 내역이 존재하지 않습니다. 채팅을 시작해보세요!";
-        return new ChatRoomListDto(chatRoom.getRoomId(), chatRoom.getName(), content);
+    public static ChatRoomListDto from(ChatRoom chatRoom, ChatMessage latestMessage, User currentUser) {
+        // 상대방 이름으로 방 이름 설정
+        String otherUserName = chatRoom.getBuyer().equals(currentUser)
+                ? chatRoom.getSeller().getNickname()
+                : chatRoom.getBuyer().getNickname();
+
+        String content = (latestMessage != null) ? latestMessage.getContent() : "채팅 내역이 없습니다.";
+
+        return new ChatRoomListDto(
+                chatRoom.getRoomId(),
+                otherUserName,
+                content
+        );
     }
 }

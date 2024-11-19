fb35c9b22fcfb458f7e38111ddab923891fd1542
seona-moon <fhflwhwl5@gmail.com>
2024-11-19 15:40:31 +0900

    fix : 에러 코드에 스택 트레이스가 뜨지 않도록 수정

--- karma/src/main/java/com/ecyce/karma/global/exception/ErrorCode.java
+++ karma/src/main/java/com/ecyce/karma/global/exception/ErrorCode.java
@@ -20,10 +20,16 @@ public enum ErrorCode {
 
     // product
     PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND , "해당하는 제품을 찾을 수 없습니다."),
+    PRODUCT_OPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 옵션을 찾을 수 없습니다."),
     INVALID_ACCESS(HttpStatus.BAD_REQUEST , "해당 사용자는 제품 정보를 수정할 수 없습니다."),
 
     // bookmark
-    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크가 존재하지 않습니다. 북마크를 등록한 후 취소할 수 있습니다.");
+    BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "북마크가 존재하지 않습니다. 북마크를 등록한 후 취소할 수 있습니다."),
+
+    // orders
+    ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 주문을 찾을 수 없습니다."),
+    ORDER_ACCESS_DENIED(HttpStatus.FORBIDDEN, "해당 주문에 접근 권한이 없습니다."),
+    ORDER_CANNOT_BE_CANCELED(HttpStatus.BAD_REQUEST, "현재 상태에서는 주문을 취소할 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;

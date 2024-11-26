2daec0385509c960bf79c3406d7ca50cddbcb495
oohyj <hyj0660@gmail.com>
2024-11-27 02:17:42 +0900

    fix : 데이터 수정

--- karma/src/main/java/com/ecyce/karma/domain/product/service/ProductService.java
+++ karma/src/main/java/com/ecyce/karma/domain/product/service/ProductService.java
@@ -56,8 +56,8 @@ public class ProductService {
         Product product = ProductRequest.toEntity(user , dto);
         productRepository.save(product);
         // 옵션이 있다면
-        if (dto.getOption() != null) {
-            for (OptionRequest optionDto : dto.getOption()) {
+        if (dto.getOptions() != null) {
+            for (OptionRequest optionDto : dto.getOptions()) {
                 ProductOption productOption = optionDto.toEntity(product , optionDto);
                 productOptionRepository.save(productOption);
             }

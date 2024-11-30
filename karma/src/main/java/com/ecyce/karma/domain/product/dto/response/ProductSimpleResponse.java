dfd1ac4b2153bbe048dee92e4782c25b9d0563c4
oohyj <hyj0660@gmail.com>
2024-11-30 21:36:23 +0900

    feat : 썸네일 추가

--- karma/src/main/java/com/ecyce/karma/domain/product/dto/response/ProductSimpleResponse.java
+++ karma/src/main/java/com/ecyce/karma/domain/product/dto/response/ProductSimpleResponse.java
@@ -11,11 +11,16 @@ public record ProductSimpleResponse(
         int price,
         int duration,
         ProductState productState,
-        boolean isMarked
-        // String thumbnail; // 나중에 썸네일 추가해야함 해당 product의 사진 중 첫번째 사진 반환
+        boolean isMarked,
+        String thumbnail // 나중에 썸네일 추가해야함 해당 product의 사진 중 첫번째 사진 반환
 ) {
 
     public static ProductSimpleResponse from(Product product , boolean isMarked){
+        String thumbnail = null;
+        if (product.getProductImages() != null && !product.getProductImages().isEmpty()) {
+            thumbnail = product.getProductImages().get(0).getProductImgUrl(); // 첫 번째 이미지의 URL 가져오기
+        }
+
         return new ProductSimpleResponse(
                 product.getProductId(),
                 product.getUser().getUserId(),
@@ -24,7 +29,8 @@ public record ProductSimpleResponse(
                 product.getPrice(),
                 product.getDuration(),
                 product.getProductState(),
-                isMarked
+                isMarked,
+                thumbnail
         );
     }
 }

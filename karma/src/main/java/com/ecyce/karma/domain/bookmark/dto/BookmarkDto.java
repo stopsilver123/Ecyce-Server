60f9041d42df6527f2806ebb03344110a1321242
stopsilver123 <sofiajieun@ewha.ac.kr>
2024-11-30 23:37:23 +0900

    feat: 회원별 북마크 목록 조회 시 productThumbnail 응답 추가

--- karma/src/main/java/com/ecyce/karma/domain/bookmark/dto/BookmarkDto.java
+++ karma/src/main/java/com/ecyce/karma/domain/bookmark/dto/BookmarkDto.java
@@ -1,6 +1,7 @@
 package com.ecyce.karma.domain.bookmark.dto;
 
 import com.ecyce.karma.domain.bookmark.entity.Bookmark;
+import com.ecyce.karma.domain.product.entity.Product;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,20 +13,28 @@ public class BookmarkDto {
     private String productName;
     private int price;
     private int duration;
+    private String productThumbnail;
 
-    public BookmarkDto(Long bookmarkId, String productName, int price, int duration) {
+    public BookmarkDto(Long bookmarkId, String productName, int price, int duration, String productThumbnail) {
         this.bookmarkId = bookmarkId;
         this.productName = productName;
         this.price = price;
         this.duration = duration;
+        this.productThumbnail = productThumbnail;
     }
 
     public static BookmarkDto from(Bookmark bookmark) {
+        Product product = bookmark.getProduct();
+        String productThumbnail = null;
+        if (product.getProductImages() != null && !product.getProductImages().isEmpty()) {
+            productThumbnail = product.getProductImages().get(0).getProductImgUrl();
+        }
         return new BookmarkDto(
                 bookmark.getBookmarkId(),
                 bookmark.getProduct().getProductName(),
                 bookmark.getProduct().getPrice(),
-                bookmark.getProduct().getDuration()
+                bookmark.getProduct().getDuration(),
+                productThumbnail
         );
     }
 }
